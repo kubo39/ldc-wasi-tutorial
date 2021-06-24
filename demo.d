2b9afe98d0d3f599c97b0f6880509abfa5b66187
Hiroki Noda <kubo39@gmail.com>
2021-06-24 23:55:38 +0900

    up

--- demo.d
+++ demo.d
@@ -3,9 +3,19 @@ nothrow:
 @nogc:
 
 int puts(const(char)*) @trusted;
+int printf(const(char)* format, ...) @trusted;
+
+pragma(crt_constructor)
+void constructor()
+{
+  puts(".init_array");
+}
+
+int tls_var = 42;
 
 int main()
 {
-  puts("hello");
+  puts("main");
+  printf("tls_var: %d\n", tls_var);
   return 0;
 }
