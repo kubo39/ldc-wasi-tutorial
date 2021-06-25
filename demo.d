9636ef8b395a4b6680cbaf6ae57a9e0b93fe0f65
Hiroki Noda <kubo39@gmail.com>
2021-06-25 20:57:24 +0900

    add malloc

--- demo.d
+++ demo.d
@@ -4,6 +4,7 @@ nothrow:
 
 int puts(const(char)*) @trusted;
 int printf(const(char)* format, ...) @trusted;
+void* malloc(size_t) @trusted;
 
 pragma(crt_constructor)
 void constructor()
@@ -17,5 +18,7 @@ int main()
 {
     puts("main");
     printf("tls_var: %d\n", tls_var);
+    auto p = malloc(0);
+    printf("malloc(3) returns %p\n", p);
     return 0;
 }
