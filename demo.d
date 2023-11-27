081988ab91e0a93d67f354acdb70b87788916319
Hiroki Noda <kubo39@gmail.com>
2023-11-28 08:53:08 +0900

    wip; add clock_gettime

--- demo.d
+++ demo.d
@@ -2,13 +2,13 @@ extern(C):
 nothrow:
 @nogc:
 
-enum RUSAGE_SELF = 1;
-
 int puts(const(char)*) @trusted;
 
 int printf(const(char)* format, ...) @trusted;
 
 void* malloc(size_t) @trusted;
+
+enum RUSAGE_SELF = 1;
 
 alias time_t = long;
 alias suseconds_t = long;
@@ -28,22 +28,19 @@ struct rusage
 // needs libwasi-emulated-process-clocks.a
 int getrusage(int, rusage*) @trusted;
 
-alias __wasi_clockid_t = uint;
-enum CLOCK_REALTIME = 0;
-enum CLOCK_MONOTONIC = 1;
 alias __wasi_timestamp_t = ulong;
 alias __wasi_errno_t = ushort;
 
-__wasi_errno_t __wasi_clock_time_get(__wasi_clockid_t, __wasi_timestamp_t, __wasi_timestamp_t*) @trusted;
+__wasi_timestamp_t __clock();
 
-__wasi_timestamp_t __clock()
-{
-    __wasi_timestamp_t ret;
-    __wasi_errno_t err = __wasi_clock_time_get(CLOCK_MONOTONIC, 0, &ret);
-    if (err != 0)
-        printf("errno: clock = %d\n", err);
-    return ret;
+alias clockid_t = int;
+enum CLOCK_MONOTONIC = 1;
+struct timespec {
+    time_t tv_sec;
+    long tv_nsec;
 }
+
+int clock_gettime(clockid_t, timespec*);
 
 pragma(crt_constructor)
 void constructor()
@@ -60,8 +57,12 @@ int __main_argc_argv(int argc, char** argv)
 
     printf("__clock: %ld\n", __clock());
 
+    timespec ts;
+    int err = clock_gettime(CLOCK_MONOTONIC, &ts);
+    printf("clock_gettime: %ld.%09ld\n", ts.tv_sec, ts.tv_nsec);
+
     rusage r_usage;
-    int err = getrusage(RUSAGE_SELF, &r_usage);
+    err = getrusage(RUSAGE_SELF, &r_usage);
     if (err != 0)
         printf("errno: getrusage: %d\n", err);
     printf("rusage(user): %4ld.%03ld\n", r_usage.ru_utime.tv_sec, r_usage.ru_utime.tv_usec / 1000);
@@ -77,6 +78,9 @@ int __main_argc_argv(int argc, char** argv)
     printf("rusage(user): %4ld.%03ld\n", r_usage2.ru_utime.tv_sec, r_usage2.ru_utime.tv_usec / 1000);
 
     printf("__clock: %ld\n", __clock());
+    timespec ts2;
+    err = clock_gettime(CLOCK_MONOTONIC, &ts2);
+    printf("clock_gettime: %ld.%09ld\n", ts2.tv_sec, ts2.tv_nsec);
 
     return 0;
 }
